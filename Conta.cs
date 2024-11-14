0bb4cc2f924e6acd613e0b5de068547eeabc4d7d
Kaique Gabriel Custódio Gonçalves <2023202060015@aluno.ifro.edu.br>
2024-11-13 20:39:26 -0400

    first commit

---
+++ Conta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Conta
+{
+    private double _saldo;
+    public void SetSaldo(double saldo)
+    {
+        try
+        {
+            if (saldo >= 0)
+            {
+                _saldo = saldo;
+            }
+            else
+            {
+                throw new Exception("O saldo não pode ser negativo!");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Conta c = new Conta();
+
+                Console.WriteLine("Informe o valor do saldo: ");
+                double saldo = double.Parse(Console.ReadLine());
+                c.SetSaldo(saldo);
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            /*
+            try
+            {
+                Console.WriteLine("Digite um valor: ");
+                int valor = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(valor / 0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Valor informado inválido.");
+            }*/
+        }
+    }
+}
